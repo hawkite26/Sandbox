7ad7c0485631038193cd4591dda3b2850b4bef22
Kye Bryce <kye-bryce@hotmail.com>
2020-09-07 02:13:52 +1000

    Update lass_students with example class coding

---
+++ class_student.py
@@ -0,0 +1,16 @@
+class Student:
+    def __init__(self, first_name="", last_name="", student_id=0):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.id = student_id
+
+    def __str__(self):
+        return "{} {} ({})".format(self.first_name, self.last_name, self.id)
+
+
+# Simple example class usage (client code)
+first_name = input("First name: ")
+last_name = input("LAst name: ")
+student_id = int(input("ID: "))
+s1 = Student(first_name, last_name, student_id)
+print(s1.first_name, "has ID", s1.id)
